3e4ccedc43adf7094a8514b0285e2d7a74431b09
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-04-23 09:09:59 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -197,7 +197,7 @@ new Thing("fractalverse",[["level 1 turtlie,40-60","level 2 turtlie,40-60","leve
 new Thing("level 1 turtlie",[]);
 for(let i=2;i<=10;i++) {
 	new Thing("level " + i + " turtlie",["level " + i-1 + " turtlie,40-60"]);
-	onsole.log(i);
+	console.log(i);
 };
 new Thing("supercluster",["galaxy cluster, 40-75","supergalaxy,25%","galaxy,100-250"]);
 new Thing("supergalaxy",["galaxy,50-200","supergalaxy,2%"]);
