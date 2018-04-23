2db1b1dc5f74bed48fce80ae1fbe5d3194c85d32
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-04-23 09:02:07 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -193,7 +193,7 @@ new Thing("megaverse",["multiverse,90-100"]);
 new Thing("multiverse",["universe,40-50","fractalverse,2-4"]);
 new Thing("universe",["hubble volume,40-60"]);
 new Thing("hubble volume",["supercluster,30-40","supergalaxy,0-3","galaxy cluster,100-200"]);
-new Thing("fractalverse",["level " + chooseRand(1,100) + " turtlie,40-60"]);
+new Thing("fractalverse",["level " + ChooseRand(1,100) + " turtlie,40-60"]);
 new Thing("level 1 turtlie",[]);
 for(let i=2;i<=100;i++) {
 	new Thing("level " + i + " turtlie",["level " + i-1 + " turtlie,40-60"]);
