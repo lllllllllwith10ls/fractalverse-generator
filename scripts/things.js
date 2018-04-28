1b67ce8a2ba4f8f53b109fd73915a3244c10f29b
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-04-28 14:08:34 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -198,7 +198,7 @@ const debug = (what) => {
 }
 new Thing("the box",["debug","altarca,90-110","the box"]);
 new Thing("debug",["debug2"],"aaabacaaaaaaa");
-new Thing("debug2",["debug!!!!"],"*PARENT*|b|c|/|a");
+new Thing("debug2",["debug!!!!"],"*PARENT*,b,c|a");
 new Thing("debug!!!!",["sublife"],"*RANDOM*");
 new Thing("altarca",["trancendentum continuum,50-60"]);
 new Thing("trancendentum continuum",["trancendentum,100-130"]);
