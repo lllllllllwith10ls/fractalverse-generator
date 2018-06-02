1416e6f6384ce6f47b08199671b8674da2f67c5f
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-06-02 12:18:38 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -383,6 +383,7 @@ new Thing("sublife phylum",["sublife class,1-3"],"phylum |*RANDOM*,letters");
 new Thing("sublife class",["sublife order,1-3"],"class |*RANDOM*,letters");
 new Thing("sublife order",["<^sublife genus,1-3"],"order  |*RANDOM*,letters");
 new Thing("<^sublife genus",["*sublife species,1-3"],"genus  |*RANDOM*,letters");
+new Thing("sublife genus",["*sublife species,1-3"],"genus  |*RANDOM*,letters");
 /*new Thing("sublife species",["sublife individual,100-300"],"*PARENT*,genus| |*RANDOM*,letters");
 new Thing("sublife individual",["cell membrane","rna","protein,2-4"],"*PARENT*| individual");*/
 new Thing("cell membrane",["phospholipid,100-200"],"plasma membrane");
