5551154d6e83f946ae8d7317a511138ae84e0524
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-06-05 13:54:05 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -242,11 +242,11 @@ Instance.prototype.Grow = function(){
 								let New = make(Things[toMakePart[0]].name, transmitter, reciever, transciever, openEVERYTHING);
 								New.parent = this;
 								this.children.push(New);
-								if (New.openEVERYTHING === true) {
+								/*if (New.openEVERYTHING === true) {
 									toggle(this.n)
 									toggle(New.n);
 									console.log(New);
-								}
+								}*/
 							}
 						}
 						if(this.transmitter === true) {
