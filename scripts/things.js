611fc1dfe25b28967132d619464acacf4ff259bf
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-06-05 16:09:56 -0500

    Update things.js

--- scripts/things.js
+++ scripts/things.js
@@ -242,6 +242,7 @@ Instance.prototype.Grow = function(){
 								New.parent = this;
 								this.children.push(New);
 								if (New.openEVERYTHING === true) {
+									toggle(this.parent.parent.n);
 									if(this.parent.display === 0) {
 										toggle(this.parent.n);
 									}
